cb54baaadffad8984e40d7a68b57fd013d128c95
GnanaKushwanthDoredla <doredlagnanakushwanth@gmail.com>
2020-07-18 16:50:51 +0530

    snowball

--- RDIP/Codes/exp4.js
+++ RDIP/Codes/exp4.js
@@ -4,6 +4,12 @@ var corpus2 = ["A wolf carried off a lamb. The lamb said,\" I know you are going
 
 var corpus3 = ["A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it.","Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said the donkey.","\"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" Then the donkey said to himself,","\"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table.","Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it ran out of the house,","and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet of the dog, and they beat me with sticks. It is not fair.\""]
 var res = "";
+define(['require','snowball'], function(require)
+{
+	var Snow = require('snowball')
+
+	stemmer = new Snowball('English');
+});
 function corpusfunc()
 {
 var x = document.getElementById("corpus").value;
@@ -171,6 +177,34 @@ document.getElementById("sub").innerHTML = "Now, consider all the tokens with th
 document.getElementById("demo2").innerHTML = "#newtypes";
 var input = '<input type="text" id="newtype">'
 document.getElementById("root").innerHTML = input;
-var button = '<button id="newtype" onclick = "newtypes">submit</button>'
+var button = '<button id="newtype" onclick = "newtypes(ele)">submit</button>'
 document.getElementById("button").innerHTML = button; 
+}
+var root_word = "";
+function newtypes(ele)
+{
+var d = 0
+var value1 = Number(document.getElementById("newtype").value);
+for(var i = 0; i < ele.length; i++)
+{
+stemmer.setCurrent(ele[i])
+stemmer.stem()			
+root_word.push(stemmer.getCurrent())
+}
+for(var i = 0; i < root_word.length; i++)
+{			
+if(root_similar[root_word[i]])
+{
+//console.log(root_similar)
+root_similar[root_word[i]]++;
+}			
+else			
+{
+				
+//console.log(root_similar)				
+d++;
+root_similar[root_word[i]] = 1;
+}
+}
+document.write(ele);
 }
